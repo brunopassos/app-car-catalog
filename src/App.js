e78879172ed63f27d87611365ac1fbf677be712a
Bruno <brunopassos_bp@hotmail.com>
2022-11-21 17:17:59 -0300

    feat: login and register pages implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
@@ -12,7 +11,7 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Login">
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Login" component={Login} />
         <Drawer.Screen name="Register" component={Register} />
