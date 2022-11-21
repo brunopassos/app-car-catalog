87cb3c1c031f49cb0a088737a6158a1d352c0954
Bruno <brunopassos_bp@hotmail.com>
2022-11-21 20:16:34 -0300

    feat: add new vehicle implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { TouchableOpacity, Text } from "react-native";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AddVehicle from "./pages/AddVehicle";
 
 const Drawer = createDrawerNavigator();
+const teste = false;
 export default function App() {
-  return (
+  return teste ? (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Login">
+      <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Login" component={Login} />
         <Drawer.Screen name="Register" component={Register} />
       </Drawer.Navigator>
     </NavigationContainer>
+  ) : (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Adicionar Veículo">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Adicionar Veículo" component={AddVehicle} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View } from "react-native";
 import CarList from "../../components/CarList";
 
 import Header from "../../components/Header";
@@ -9,7 +9,7 @@ import FilterOptions from "../../components/FilterOption";
 export default function Home() {
   return (
     <View style={{ flex: 1 }}>
-      <Header />
+      <Header/>
       <SearchBar />
       <FilterOptions/>
       <CarList />
