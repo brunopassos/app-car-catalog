11a088f9f2b847f02d322e8ee235723dc1f1b1b1
Bruno <brunopassos_bp@hotmail.com>
2022-11-20 21:04:57 -0300

    feat: menu navigation

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home(){
+    return(
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text style={{fontSize: 35}}>Tela Home</Text>
+        </View>
+    )
+}
+
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Register(){
+    return(
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text style={{fontSize: 35}}>Tela Register</Text>
+        </View>
+    )
+}
