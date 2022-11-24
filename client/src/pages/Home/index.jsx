ab38dd879b49ed2d0af33ddbcd4e1664175ee689
Bruno <brunopassos_bp@hotmail.com>
2022-11-24 20:32:51 -0300

    fix: home data get fixed

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,11 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { View } from "react-native";
 import CarList from "../../components/CarList";
 
 import Header from "../../components/Header";
 import SearchBar from "../../components/searchBar";
+import { AuthContext } from "../../context/auth";
 
 export default function Home() {
+
+  const { fetchData } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
     <View style={{ flex: 1 }}>
       <Header />
