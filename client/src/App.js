4181fe5c9b9d4faaf86427685dd892530129b0f8
Bruno <brunopassos_bp@hotmail.com>
2022-11-25 14:17:18 -0300

    feat: added toast

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import AuthProvider from "./context/auth";
 import Routes from "./routes";
 import { LogBox } from "react-native";
+import Toast from 'react-native-toast-message';
 
 
 export default function App() {
   LogBox.ignoreAllLogs()
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </NavigationContainer>
+      <Toast />
+    </>
   );
 }
