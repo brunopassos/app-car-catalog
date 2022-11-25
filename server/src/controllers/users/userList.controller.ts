dfc50b40655e6e4858aea1a227ed60a12f5ed3de
Bruno <brunopassos_bp@hotmail.com>
2022-11-24 21:56:11 -0300

    feat: hide password implemented

--- server/src/controllers/users/userList.controller.ts
+++ server/src/controllers/users/userList.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { AppError, handleError } from "../../errors/appError";
 import userListService from "../../services/users/userList.service";
+import { instanceToInstance } from "class-transformer";
 
 const userListController = async (req: Request, res: Response) => {
   try {
     const users = await userListService();
-    return res.send(users).status(200);
+    return res.send(instanceToInstance(users)).status(200);
   } catch (error) {
     if (error instanceof AppError) {
       handleError(error, res);
